5b24aa6a2a0fca711f0320e237da98a8265c047a
hakan-gul <9hakangl@gmail.com>
2024-03-08 23:41:42 +0300

    css-updated

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './quiz.css'
+import {data} from "../../assets/data.js"
 
 const Quiz = () => {
+   let [index, setIndex] = useState(0);
+   let [question, setQuestion] = useState(data[index]);
+   let [trueQuestion, setTrueQuestion] = useState(1);
+   let [lock, setLock] = useState(false);
+
+   const checkAns = (ans) =>{
+    if (lock === false){
+      if(question.ans==ans){
+      setTrueQuestion(trueQuestion + 1);
+      setLock(true);
+      console.log(trueQuestion)
+    }
+    }
+   }
   return (
     <div className='container'>
         <h1>Quiz App</h1>
         <hr />
-        <h2>1. Which device is required for the internet connection?</h2>
+        <h2>{index + 1} {question.question}</h2>
         <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>Lan Cable</li>
-            <li>Pen Drive</li>
+            <li onClick={()=>{checkAns(1)}}>{question.option1}</li>
+            <li onClick={()=>{checkAns(2)}}>{question.option2}</li>
+            <li onClick={()=>{checkAns(3)}}>{question.option3}</li>
+            <li onClick={()=>{checkAns(4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
