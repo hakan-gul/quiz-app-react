f2b14e1bf4ac445ab869b5a8ccbc3eb0e40f40c3
hakan-gul <9hakangl@gmail.com>
2024-03-11 00:00:07 +0300

    useref-added

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -8,12 +8,22 @@ const Quiz = () => {
    let [trueQuestion, setTrueQuestion] = useState(1);
    let [lock, setLock] = useState(false);
 
+   let Option1 = useRef(null);
+   let Option2 = useRef(null);
+   let Option3 = useRef(null);
+   let Option4 = useRef(null);
+
+   let option_array = [Option1,Option2,Option3,Option4];
+
    const checkAns = (ans) =>{
     if (lock === false){
       if(question.ans==ans){
       setTrueQuestion(trueQuestion + 1);
       setLock(true);
       console.log(trueQuestion)
+    }else{
+      setLock(true);
+      option_array[question.ans-1].current.classList.add("correct");
     }
     }
    }
@@ -23,10 +33,10 @@ const Quiz = () => {
         <hr />
         <h2>{index + 1} {question.question}</h2>
         <ul>
-            <li onClick={()=>{checkAns(1)}}>{question.option1}</li>
-            <li onClick={()=>{checkAns(2)}}>{question.option2}</li>
-            <li onClick={()=>{checkAns(3)}}>{question.option3}</li>
-            <li onClick={()=>{checkAns(4)}}>{question.option4}</li>
+            <li ref={Option1} onClick={()=>{checkAns(1)}}>{question.option1}</li>
+            <li ref={Option2} onClick={()=>{checkAns(2)}}>{question.option2}</li>
+            <li ref={Option3} onClick={()=>{checkAns(3)}}>{question.option3}</li>
+            <li ref={Option4} onClick={()=>{checkAns(4)}}>{question.option4}</li>
         </ul>
         <button>Next</button>
         <div className="index">1 of 5 questions</div>
