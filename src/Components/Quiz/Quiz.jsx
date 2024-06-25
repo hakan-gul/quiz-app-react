799602d191c6a238fa357f5c04b410cb9a545112
hakan-gul <9hakangl@gmail.com>
2024-06-26 01:58:16 +0300

    quiz-app-design-change

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -53,22 +53,36 @@ const Quiz = () => {
     setResult(false);
   }
   return (
-    <div className='container'>
-        <h1>Quiz App</h1>
-        <hr />
-        
-        {result?<>skorunuz %{score*20} tebrikler
-        <button onClick={reset}>Tekrar Başlat</button> </>:<><h2>{index + 1} {question.question}</h2>
-        <ul>
-            <li ref={Option1} onClick={()=>{checkAns(1)}}>{question.option1}</li>
-            <li ref={Option2} onClick={()=>{checkAns(2)}}>{question.option2}</li>
-            <li ref={Option3} onClick={()=>{checkAns(3)}}>{question.option3}</li>
-            <li ref={Option4} onClick={()=>{checkAns(4)}}>{question.option4}</li>
-        </ul>
-        <button onClick={next}>Next</button>
-        <div className="index">{index+1} of {data.length} questions</div></>}
-        
+    <>
+    <div className='h-screen bg-sky-200'>
+      <h1 className='text-5xl text-center pt-20 pb-5'>Depresyon Testi</h1>
+      <div className='flex justify-center items-center'>
+        <div className='py-16 px-10 rounded-lg lg:w-1/3 md:w-2/3 bg-white mt-4 flex justify-center items-center mx-3'>
+          <div className='container'>
+              {result?<>
+              <div className='flex items-center justify-center'>
+                <div className='font-bold text-xl mb-7'>
+                  skorunuz %{score*20} tebrikler
+                </div>
+              </div>
+              <div className='flex items-center justify-center'>
+                <button onClick={reset}>Tekrar Başlat</button><button>Anasayfa</button></div></>:<><h2 className='pb-4'>{index + 1} {question.question}</h2>
+              <ul>
+                  <li ref={Option1} onClick={()=>{checkAns(1)}}>{question.option1}</li>
+                  <li ref={Option2} onClick={()=>{checkAns(2)}}>{question.option2}</li>
+                  <li ref={Option3} onClick={()=>{checkAns(3)}}>{question.option3}</li>
+                  <li ref={Option4} onClick={()=>{checkAns(4)}}>{question.option4}</li>
+              </ul>
+              <div className='flex items-center justify-center'>
+                <button onClick={next}>Next</button>
+              </div>
+              <div className="index flex items-center justify-center pt-3">{index+1} / {data.length} Soru</div></>}
+          </div>
+        </div>
+      </div>
     </div>
+    </>
+    
   )
 }
 
